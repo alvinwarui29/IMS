c7b190a65a8352c7cda19038af6fdd08dcea8477
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2024-08-01 00:53:11 +0300

    added category and items resource

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -19,10 +19,20 @@ export default function Authenticated({ user, header, children }) {
                                     <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                                 </Link>
                             </div>
-
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
+                                </NavLink>
+                            </div>
+
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink href={route('category.index')} active={route().current('category.index')}>
+                                   Category
+                                </NavLink>
+                            </div>
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                                <NavLink href={route('item.index')} active={route().current('item.index')}>
+                                    Item
                                 </NavLink>
                             </div>
                         </div>
